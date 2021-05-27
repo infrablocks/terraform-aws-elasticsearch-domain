239da52973714955a9812292e0844641df049619
Toby Clemson <tobyclemson@gmail.com>
2021-05-27 14:37:50 +0100

    Fix reference to incorrect variable name.

--- security_groups.tf
+++ security_groups.tf
@@ -5,7 +5,7 @@ data "aws_vpc" "network" {
 resource "aws_security_group" "elasticsearch" {
   name = "es-${var.component}-${var.deployment_identifier}"
   vpc_id = var.vpc_id
-  description = "Elasticsearch domain for component: ${var.component}, domain: ${var.name}, deployment: ${var.deployment_identifier}"
+  description = "Elasticsearch domain for component: ${var.component}, domain: ${var.elasticsearch_domain_name}, deployment: ${var.deployment_identifier}"
 
   ingress {
     from_port = 0
