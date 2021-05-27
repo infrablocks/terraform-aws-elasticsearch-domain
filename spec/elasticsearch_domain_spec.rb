60a9a4d0e5fdad74fc48585a573fa56706432877
Toby Clemson <tobyclemson@gmail.com>
2021-05-27 19:28:47 +0100

    Lookup elasticsearch domain by name.

--- spec/elasticsearch_domain_spec.rb
+++ spec/elasticsearch_domain_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe 'elasticsearch domain' do
   include_context :terraform
 
-  let(:elasticsearch_domain_arn) { output_for(:harness, "elasticsearch_domain_arn") }
+  let(:elasticsearch_domain_name) { output_for(:harness, "elasticsearch_domain_name") }
 
   subject {
-    elasticsearch(elasticsearch_domain_arn)
+    elasticsearch(elasticsearch_domain_name)
   }
 
   it { should exist }
